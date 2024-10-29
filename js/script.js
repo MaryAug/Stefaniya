62c9c3319a6ea39c80edff58aa5c8723579ef2b3
Mary <martmary566@gmail.com>
2024-10-29 11:46:13 +0300

    clean code

--- js/script.js
+++ js/script.js
@@ -7,11 +7,7 @@ var swiper2 = new Swiper(".reviews__swiper-wrapper", {
   },
   grabCursor: true,
 
-  //Кол-во слайдов для показа
-  // slidesPerView: 3,
-  //Кол-во слайдов для пролистывания
   slidesPerGroup: 1,
-  // Responsive breakpoints
   breakpoints: {
     0: {
       slidesPerView: 1,
@@ -30,13 +26,8 @@ var swiper2 = new Swiper(".reviews__swiper-wrapper", {
 
 var swiper = new Swiper(".rooms__images", {
   slidesPerView: 1.5,
-  // loop: true,
   spaceBetween: 20,
   grabCursor: true,
-  // По идее, убирает накладывание изображений друг на друга
-  // updateOnWindowResize: true,
-  // effect: "creative",
-  //Переход к слайду по клику
   slideToClickedSlide: true,
   breakpoints: {
     0: {
@@ -53,13 +44,6 @@ var swiper = new Swiper(".rooms__images", {
     },
   },
 });
-
-// document
-//   .querySelector(".header__burger")
-//   .addEventListener("click", function () {
-//     this.classList.toggle("active");
-//     document.querySelector(".header__nav").classList.toggle("open");
-//   });
 
 const headerBurger = document.querySelector(".header__burger");
 const headerNav = document.querySelector(".header__nav");
